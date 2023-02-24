b2ddbce1cbf5477c3c6e184ed22e3391028c7627
Enbiya Göral <100806254+enbiyagoral@users.noreply.github.com>
2023-02-24 22:08:04 +0300

    Course Model

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const pageRoute = require('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoute')
+
+
+
 const app = express();
 
-const port = 3000;
+mongoose.connect('mongodb://127.0.0.1/smartedu-db')
+.then(()=>{
+  console.log('DB Connected Successfuly!');
+});
+
+
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -11,7 +21,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use('/', pageRoute );
+app.use('/courses', courseRoute );
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
