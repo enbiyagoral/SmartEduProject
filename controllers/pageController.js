54363d94ae7d9149f711d12861389d33cbdf9005
Enbiya Göral <100806254+enbiyagoral@users.noreply.github.com>
2023-03-04 21:18:39 +0300

    Login Page

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -16,3 +16,9 @@ exports.getRegisterPage = (req, res) => {
   });
 };
 
+exports.getLoginPage = (req, res) => {
+  res.status(200).render('login', {
+    page_name: 'login', //  navigation barı hazırla // page route'a logini ekle // 
+  });
+};
+
